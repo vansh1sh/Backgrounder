0744cb43c4eed9b802a56b802d2e45dea5420000
Vansh Badkul <vanshbadkul@gmail.com>
2016-09-27 23:36:18 +0530

    virus

---
+++ app/src/main/java/com/example/vansh/backgrounder/RecorderService.java
@@ -0,0 +1,173 @@
+/*
+ * Copyright (c) 2015, Picker Weng
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ *  Neither the name of CameraRecorder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Project:
+ *     CameraRecorder
+ *
+ * File:
+ *     CameraRecorder.java
+ *
+ * Author:
+ *     Picker Weng (pickerweng@gmail.com)
+ */
+
+package com.example.vansh.backgrounder;
+
+import android.app.Service;
+import android.content.Intent;
+import android.hardware.Camera;
+import android.hardware.Camera.Size;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+
+public class RecorderService extends Service {
+	private static final String TAG = "RecorderService";
+	private SurfaceView mSurfaceView;
+	private SurfaceHolder mSurfaceHolder;
+	private static Camera mServiceCamera;
+	private boolean mRecordingStatus;
+	private MediaRecorder mMediaRecorder;
+
+	@Override
+	public void onCreate() {
+		mRecordingStatus = false;
+		mServiceCamera = CameraRecorder.mCamera;
+		mSurfaceView = CameraRecorder.mSurfaceView;
+		mSurfaceHolder = CameraRecorder.mSurfaceHolder;
+
+		super.onCreate();
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		super.onStartCommand(intent, flags, startId);
+
+		if (mRecordingStatus == false)
+			startRecording();
+
+		return START_STICKY;
+	}
+
+	@Override
+	public void onDestroy() {
+		stopRecording();
+		mRecordingStatus = false;
+
+		super.onDestroy();
+	}
+
+	public boolean startRecording(){
+		try {
+			Toast.makeText(getBaseContext(), "Recording Started", Toast.LENGTH_SHORT).show();
+			mServiceCamera = Camera.open();
+			Camera.Parameters params = mServiceCamera.getParameters();
+			mServiceCamera.setParameters(params);
+			Camera.Parameters p = mServiceCamera.getParameters();
+
+			final List<Size> listPreviewSize = p.getSupportedPreviewSizes();
+			for (Size size : listPreviewSize) {
+                Log.i(TAG, String.format("Supported Preview Size (%d, %d)", size.width, size.height));
+            }
+
+            Size previewSize = listPreviewSize.get(0);
+			p.setPreviewSize(previewSize.width, previewSize.height);
+			mServiceCamera.setParameters(p);
+
+			try {
+				mServiceCamera.setPreviewDisplay(mSurfaceHolder);
+				mServiceCamera.startPreview();
+			}
+			catch (IOException e) {
+				Log.e(TAG, e.getMessage());
+				e.printStackTrace();
+			}
+
+			mServiceCamera.unlock();
+
+			mMediaRecorder = new MediaRecorder();
+			mMediaRecorder.setCamera(mServiceCamera);
+			mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+			mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+			mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+			mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+			mMediaRecorder.setOutputFile(Environment.getExternalStorageDirectory().getPath() + "/video.mp4");
+			mMediaRecorder.setPreviewDisplay(mSurfaceHolder.getSurface());
+
+			mMediaRecorder.prepare();
+			mMediaRecorder.start();
+
+			mRecordingStatus = true;
+
+			return true;
+
+		} catch (IllegalStateException e) {
+			Log.d(TAG, e.getMessage());
+			e.printStackTrace();
+			return false;
+
+		} catch (IOException e) {
+			Log.d(TAG, e.getMessage());
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public void stopRecording() {
+		Toast.makeText(getBaseContext(), "Recording Stopped", Toast.LENGTH_SHORT).show();
+		try {
+			mServiceCamera.reconnect();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		mMediaRecorder.stop();
+		mMediaRecorder.reset();
+
+		mServiceCamera.stopPreview();
+		mMediaRecorder.release();
+
+		mServiceCamera.release();
+		mServiceCamera = null;
+	}
+}
